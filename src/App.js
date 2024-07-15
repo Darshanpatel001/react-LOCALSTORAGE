ba2a98234303d9b9fcd658eccc7152f2e3146e54
Darshanpatel001 <darshanpatel4298@gmail.com>
2024-07-15 14:27:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MobileList from './componant/MobileList';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <MobileList/>
+    </div>
+  );
+}
+
+export default App;
